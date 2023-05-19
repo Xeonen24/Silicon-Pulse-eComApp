b5d6e16f95ddeace3d8921ed8a64d8669f859d08
IWhitebird <115157819+IWhitebird@users.noreply.github.com>
2023-05-19 16:25:58 +0530

    a

--- src/Components/Static/Footer/Footer.jsx
+++ src/Components/Static/Footer/Footer.jsx
@@ -7,9 +7,10 @@ const Footer = () => {
       <div className="container">
         <p className="text">© 2023 Your Company. All rights reserved.</p>
         <p className="text">Contact: example@example.com</p>
-      </div>
+      </div> 
     </footer>
   );
 };
 
 export default Footer;
+
