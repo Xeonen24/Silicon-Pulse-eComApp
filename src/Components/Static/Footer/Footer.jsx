c41683fddaef950aa49b6f4f2ffd565641e54b17
IWhitebird <115157819+IWhitebird@users.noreply.github.com>
2023-05-19 15:26:14 +0530

    aa

--- src/Components/Static/Footer/Footer.jsx
+++ src/Components/Static/Footer/Footer.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import '../Footer/footer.css'; // Import the CSS file
 
 const Footer = () => {
   return (
-    <footer style={footerStyle}>
-      <div style={containerStyle}>
-        <p style={textStyle}>© 2023 Your Company. All rights reserved.</p>
-        <p style={textStyle}>Contact: example@example.com</p>
+    <footer className="footer">
+      <div className="container">
+        <p className="text">© 2023 Your Company. All rights reserved.</p>
+        <p className="text">Contact: example@example.com</p>
       </div>
     </footer>
   );
 };
-
 
 export default Footer;
