7e699ce17895895873c7b3fbf2245f956092c951
IWhitebird <115157819+IWhitebird@users.noreply.github.com>
2023-05-19 15:16:13 +0530

    a
    a

--- src/Components/Static/Footer/Footer.jsx
+++ src/Components/Static/Footer/Footer.jsx
@@ -11,22 +11,5 @@ const Footer = () => {
   );
 };
 
-const footerStyle = {
-  backgroundColor: '#f0f0f0',
-  padding: '20px',
-  marginTop: '50px',
-  textAlign: 'center'
-};
-
-const containerStyle = {
-  maxWidth: '800px',
-  margin: '0 auto'
-};
-
-const textStyle = {
-  color: '#888',
-  fontSize: '14px',
-  margin: '5px'
-};
 
 export default Footer;
