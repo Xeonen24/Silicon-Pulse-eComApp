aabccd39e5760f18fd73e576e5e3eb7ede8ddc0b
Neepun Patil <pneepun@gmail.com>
2023-05-20 13:13:26 +0530

    Ipad

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -6,17 +6,18 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div>
-            <h1 className="main_name">Silicon Pulses </h1>
-            <p className="subhead_name">
-                The Ultimate Gaming Hardware Emporium.
-            </p>
-            <Link to='product'><h4 className='home_btn'>Shop Now</h4></Link>
-            <img
-                className="img_main"
-                src={image}
-                alt=""
-            />           
-
+            <div className="main_div">
+                <h1 className="main_name">Silicon Pulses </h1>
+                <p className="subhead_name">
+                    The Ultimate Gaming Hardware Empssorium.
+                </p>
+                <Link to='product'><h4 className='home_btn'>Shop Now</h4></Link>
+                <img
+                    className="img_main"
+                    src={image}
+                    alt=""
+                />
+            </div>           
         </div>
     );
 }
