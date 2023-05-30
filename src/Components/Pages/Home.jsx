ab28e198609e0cc0c788dc9c857a56e6c394a505
Neepun Patil <pneepun@gmail.com>
2023-05-30 18:47:41 +0530

    kek

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -17,7 +17,6 @@ const Home = () => {
                     alt=""
                 />
             </div>           
-    );
+    )
 }
-
 export default Home;
--- src/Components/Pages/Product/Product.jsx
+++ src/Components/Pages/Product/Product.jsx
@@ -1,10 +1,64 @@
 import React from 'react';
+import './product.css';
+import image from "../../../Images/intel.jpg";
 
 const Product = () =>{
     return ( 
         <>
+            <div className="grid">
+                <div  className="grid-item">
+                <img src={image} alt="bate" className="item-image" />
+                <h3 className="item-title">Intel Core i7 bate</h3>
+                <p className="item-price">400$</p>
+                </div>
+                <div  className="grid-item">
+                <img src={image} alt="bate" className="item-image" />
+                <h3 className="item-title"></h3>
+                <p className="item-price">400$</p>
+                </div>
+                <div  className="grid-item">
+                <img src={image} alt="bate" className="item-image" />
+                <h3 className="item-title"></h3>
+                <p className="item-price">400$</p>
+                </div>
+                <div  className="grid-item">
+                <img src={image} alt="bate" className="item-image" />
+                <h3 className="item-title"></h3>
+                <p className="item-price">400$</p>
+                </div>
+                <div  className="grid-item">
+                <img src={image} alt="bate" className="item-image" />
+                <h3 className="item-title"></h3>
+                <p className="item-price">400$</p>
+                </div>
+                <div  className="grid-item">
+                <img src={image} alt="bate" className="item-image" />
+                <h3 className="item-title"></h3>
+                <p className="item-price">400$</p>
+                </div>
+                <div  className="grid-item">
+                <img src={image} alt="bate" className="item-image" />
+                <h3 className="item-title"></h3>
+                <p className="item-price">400$</p>
+                </div>
+                <div  className="grid-item">
+                <img src={image} alt="bate" className="item-image" />
+                <h3 className="item-title"></h3>
+                <p className="item-price">400$</p>
+                </div>
+                <div  className="grid-item">
+                <img src={image} alt="bate" className="item-image" />
+                <h3 className="item-title"></h3>
+                <p className="item-price">400$</p>
+                </div>
+                <div  className="grid-item">
+                <img src={image} alt="bate" className="item-image" />
+                <h3 className="item-title"></h3>
+                <p className="item-price">400$</p>
+                </div>
+            </div>
         </>
-     );
+    );
 }
 
 export default Product;
