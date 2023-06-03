9c0a397c068b5beeff16cd7d57060733888fd1a2
Alpesh Pote <captainsparkelz32@gmail.com>
2023-06-03 15:41:29 +0530

    asd

--- src/Components/Static/Header/Header.jsx
+++ src/Components/Static/Header/Header.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./header.css";
 
 function Navbar() {
@@ -16,43 +17,64 @@ function Navbar() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-white bg-white nav-bar-all">
-      <a className="navbar-brand" href="#">Navbar Placeholder</a>
+      <a className="navbar-brand" href="#">
+        Navbar Placeholder
+      </a>
       <button
-        className={`navbar-toggler ${expanded ? '' : 'collapsed'}`}
+        className={`navbar-toggler ${expanded ? "" : "collapsed"}`}
         type="button"
         onClick={toggleNavbar}
         aria-expanded={expanded}
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className={`collapse navbar-collapse ${expanded ? 'show' : ''}`}>
+      <div className={`collapse navbar-collapse ${expanded ? "show" : ""}`}>
         <ul className="navbar-nav mr-auto">
           <li className="nav-item nav-homya nav-comp">
-            <a className="nav-link  " href="#">Home</a>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
           </li>
-          <li className={`nav-item dropdown nav-comp ${dropdownOpen ? 'show' : ''}`}>
-            <a className="nav-link dropdown-toggle nav-comp nav-drops" href="#" onClick={toggleDropdown}>
+          <li
+            className={`nav-item dropdown nav-comp ${
+              dropdownOpen ? "show" : ""
+            }`}
+          >
+            <a
+              className="nav-link dropdown-toggle nav-comp nav-drops"
+              href="#"
+              onClick={toggleDropdown}
+            >
               Products
             </a>
             <div
-              className={`dropdown-menu ${dropdownOpen ? 'show' : ''}`}
+              className={`dropdown-menu ${dropdownOpen ? "show" : ""}`}
               style={{
-                maxHeight: dropdownOpen ? '500px' : '0',
-                opacity: dropdownOpen ? '1' : '0',
-                transition: 'max-height 0.5s ease, opacity 0.5s ease',
+                maxHeight: dropdownOpen ? "500px" : "0",
+                opacity: dropdownOpen ? "1" : "0",
               }}
             >
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <a className="dropdown-item" href="#">Something else here</a>
+              <a className="dropdown-item" href="#">
+                Action
+              </a>
+              <a className="dropdown-item" href="#">
+                Another action
+              </a>
+              <a className="dropdown-item" href="#">
+                Something else here
+              </a>
             </div>
           </li>
-          
+
           <li className="nav-item nav-comp">
-            <a className="nav-link " href="#">About</a>
+            <a className="nav-link " href="#">
+              About
+            </a>
           </li>
           <li className="nav-item nav-comp nav-p">
-            <a className="nav-link" href="#">Pricing</a>
+            <a className="nav-link" href="#">
+              Pricing
+            </a>
           </li>
         </ul>
       </div>
@@ -60,4 +82,3 @@ function Navbar() {
   );
 }
 export default Navbar;
-  
