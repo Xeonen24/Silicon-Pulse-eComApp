d50fdd176248d174874246a0f59edd311248b012
IWhitebird <patangeshreyas@gmail.com>
2023-06-29 17:57:23 +0530

    yoo

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import image from '../../Images/hs.png'
-import './home.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import image from "../../Images/hs.png";
+import "./home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            <h1 className="main_name">Silicon Pulse </h1>
-            <p className="subhead_name">
-                The Ultimate Gaming Hardware Emporium.
-            </p>
-            <Link to='product'><h4 className='home_btn'>Shop Now</h4></Link>
-            <img
-                className="img_main"
-                src={image}
-                alt=""
-            />           
+  return (
+    <div className="wrapper">
 
-        </div>
-    );
-}
 
+      <div className="main_div">
+       
+        <h1 className="main_name">Silicon Pulses </h1>
+        <p className="subhead_name">The Ultimate Gaming Hardware Empssorium.</p>
+        <Link className= "shop-now-link" to="product">
+            <h4 className="home_btn button-64">
+              Shop Now
+            </h4>
+        </Link>
+     
+      </div>
+
+  </div>
+  );
+};
 export default Home;
