355855afb3774ca7d567e9326f69878096e6dfc1
Alpesh Pote <captainsparkelz32@gmail.com>
2023-08-07 18:10:34 +0530

    h

--- src/Components/Pages/Profile/Profile.jsx
+++ src/Components/Pages/Profile/Profile.jsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import userLogo from '../../../Images/user.png';
-import './profile.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import userLogo from "../../../Images/user.png";
+import { Link } from "react-router-dom";
+import "./profile.css";
 
 const Profile = () => {
   const [user, setUser] = useState(null);
-  const [formattedDate, setFormattedDate] = useState('');
+  const [formattedDate, setFormattedDate] = useState("");
   const [editMode, setEditMode] = useState(false);
-  const [newusername, setUsername] = useState('');
-  const [previousPassword, setPreviousPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
+  const [newusername, setUsername] = useState("");
+  const [previousPassword, setPreviousPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
   useEffect(() => {
-    const userData = localStorage.getItem('userDetails');
+    const userData = localStorage.getItem("userDetails");
 
     if (userData) {
       const parsedUserData = JSON.parse(userData);
@@ -20,7 +21,7 @@ const Profile = () => {
 
       const createdAt = parsedUserData.createdAt;
       const date = new Date(createdAt);
-      const formattedDate = date.toLocaleDateString('en-GB');
+      const formattedDate = date.toLocaleDateString("en-GB");
       setFormattedDate(formattedDate);
     }
   }, []);
@@ -36,58 +37,62 @@ const Profile = () => {
       previousPassword,
       newPassword,
     };
-  
+
     axios
-      .post('http://localhost:5000/api/update-profile', updatedProfile, {
+      .post("http://localhost:5000/api/update-profile", updatedProfile, {
         withCredentials: true,
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       })
-      .then(response => {
+      .then((response) => {
         setUser(response.data.user);
         setEditMode(false);
-        setPreviousPassword('');
-        setNewPassword('');
-  
-        window.alert('Profile updated successfully. Please re-login.');
+        setPreviousPassword("");
+        setNewPassword("");
+
+        window.alert("Profile updated successfully. Please re-login.");
         setTimeout(() => {
-          logoutUser(); 
+          logoutUser();
         }, 3000);
       })
-      .catch(error => {
-        if (error.response && error.response.data && error.response.data.error) {
+      .catch((error) => {
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.error
+        ) {
           window.alert(error.response.data.error);
         } else {
-          console.error('Error updating profile:', error);
+          console.error("Error updating profile:", error);
         }
       });
   };
-  
+
   const logoutUser = async () => {
     try {
       const response = await axios.post(
-        'http://localhost:5000/api/logout',
+        "http://localhost:5000/api/logout",
         {},
         {
           withCredentials: true,
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         }
       );
-      localStorage.removeItem('userDetails');
-      window.location.href = '/'; 
+      localStorage.removeItem("userDetails");
+      window.location.href = "/";
     } catch (error) {
       console.error(error);
     }
   };
-  
+
   const handleCancelEdit = () => {
     setEditMode(false);
     setUsername(user.newusername);
-    setPreviousPassword('');
-    setNewPassword('');
+    setPreviousPassword("");
+    setNewPassword("");
   };
 
   if (!user) {
@@ -95,69 +100,61 @@ const Profile = () => {
   }
 
   return (
-    <div className="profile">
-      <div className="profile-header">
-        <h2 className="profile-title">Your Profile</h2>
-      </div>
-      <div className="profile-content">
-        <div className="profile-image">
-          <img src={userLogo} alt="User Profile" />
-        </div>
-        <div className="profile-details">
-          {editMode ? (
-            <>
-              <input
-                type="text"
-                value={newusername}
-                onChange={e => setUsername(e.target.value)}
-                placeholder="Username"
-                className="profile-input"
-              />
-              <input
-                type="password"
-                value={previousPassword}
-                onChange={e => setPreviousPassword(e.target.value)}
-                placeholder="Previous Password"
-                className="profile-input"
-              />
-              <input
-                type="password"
-                value={newPassword}
-                onChange={e => setNewPassword(e.target.value)}
-                placeholder="New Password"
-                className="profile-input"
-              />
-            </>
-          ) : (
-            <>
-              <h2 className="profile-name">{user.username}</h2>
-              <p className="profile-email">Email: {user.email}</p>
-              <p className="profile-location">Profile Created: {formattedDate}</p>
-            </>
-          )}
-          {editMode ? (
-            <div>
-              <button className="profile-save-button" onClick={handleSaveProfile}>
-                Save
-              </button>
-              <button className="profile-cancel-button" onClick={handleCancelEdit}>
-                Cancel
-              </button>
+    <div style={{marginTop:'10rem'}}>
+      <div className="profile">
+        <div className="sidenav">
+          <div className="profile">
+            <img src={userLogo} alt="" width="80px" height="80px" />
+          </div>
+          <div className="name">{user.username}</div>
+          <div className="sidenav-url">
+            <div className="url">
+              <Link to="/profile" className="active">
+                Profile
+              </Link>
             </div>
-          ) : (
-            <button className="profile-edit-button" onClick={handleEditProfile}>
-              Edit Profile
-            </button>
-          )}
+            <div className="url">
+              <a href="#settings">Settings</a>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="profile-orders">
-        <h3 className="profile-section-title">Order History</h3>
-        <div className="profile-order-item">
-          <p className="profile-order-id">Order ID:</p>
-          <p className="profile-order-date">Order Date:</p>
-          <p className="profile-order-total">Order Total: </p>
-          <button className="profile-order-details-button">View Details</button>
+
+        <div className="main">
+          <div className="card">
+            <h2 className="mainh2">IDENTITY</h2>
+            <div className="card-body">
+              <i className="fa fa-pen fa-xs edit"></i>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Name</td>
+                    <td>:</td>
+                    <td>{user.username}</td>
+                  </tr>
+                  <tr>
+                    <td>Email</td>
+                    <td>:</td>
+                    <td>{user.email}</td>
+                  </tr>
+                  <tr>
+                    <td>Address</td>
+                    <td>:</td>
+                    <td>.</td>
+                  </tr>
+                  <tr>
+                    <td>Mobile</td>
+                    <td>:</td>
+                    <td>.</td>
+                  </tr>
+                  <tr>
+                    <td>Country</td>
+                    <td>:</td>
+                    <td>.</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
