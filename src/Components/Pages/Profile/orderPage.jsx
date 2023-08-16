197b62aaabb6cb9ce456363fb43095057d28c50f
Alpesh Pote <captainsparkelz32@gmail.com>
2023-08-16 17:55:07 +0530

    asdasddas

--- src/Components/Pages/Profile/orderPage.jsx
+++ src/Components/Pages/Profile/orderPage.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './OrderPage.css'; // Import your CSS file for styling
-import { width } from '@mui/system';
+import { Link } from 'react-router-dom';
+import './OrderPage.css';
+import { Line, Circle } from 'rc-progress';
 import moment from 'moment';
 
 const OrderPage = () => {
   const [orders, setOrders] = useState([]);
-  
 
   useEffect(() => {
     const getOrders = async () => {
@@ -35,26 +35,33 @@ const OrderPage = () => {
             <div className="order-details">
               <p className="order-id">Order ID: {order._id}</p>
               <p className="order_date">Order date: {moment(order.createdAt).format("DD MMM YYYY")}</p>
-              <p className="total-price">Total Price: ${order.totalPrice}</p>
               <p className="payment-method">Payment Method: {order.paymentMethod}</p>
             </div>
             <div className="order-status">
+            <Line percent={12} strokeWidth={1} strokeColor="red" />
               <p className="status">Status: {order.shipped}</p>
             </div>
-              <details className="learn-more">
+            <details className="learn-more">
                 <summary>View Items</summary>
-                <ul>
-                  {order.products.map((product, index) => (
+              <ul>
+                {order.products.map((product, index) => (
+                  <>
                     <li key={index} >
-                      <strong><img src={product.product.imagePath} style={ {marginLeft:"1rem",marginTop:"3rem",height:"180px" ,width : "180px" }}></img></strong>
-                      <h4 className='prod_title'>Title:{product.product.title}</h4>
-                      <h4 className='prod_price'>Price:${product.product.price}</h4>
-                      <h4 className='prod_quantity'>Quantity:{product.quantity}</h4>
+                      <div style={{ marginTop: '1rem', maxHeight: '210px' }}>
+                        <strong><img src={product.product.imagePath} style={{ marginLeft: "1rem", marginTop: "1rem", height: "180px", width: "180px" }}></img></strong>
+                        <Link style={{ textDecoration: 'none' }} to={`/product/${product.product._id}`}>
+                          <h4 className='prod_title'>{product.product.title}</h4>
+                        </Link>
+                        <b className='prod_price'>Price: ₹ {product.product.price}</b>
+                        <h4 className='prod_quantity'>Quantity: {product.quantity}</h4>
+                      </div>
                     </li>
-                  ))}
-                </ul>
-                <p className="total-price-drop">Total: ${order.totalPrice}</p>
-              </details>
+                    <hr />
+                  </>
+                ))}
+              </ul>
+              <p className="total-price-drop">Total: ₹ {order.totalPrice}</p>
+            </details>
           </div>
         ))}
       </div>
