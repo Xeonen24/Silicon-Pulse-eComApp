26d1e2766201b697f05553d6af0324519bca2594
IWhitebird <115157819+IWhitebird@users.noreply.github.com>
2023-09-01 20:13:49 +0530

    Update authcontroller.js

--- server/routes/authcontroller.js
+++ server/routes/authcontroller.js
@@ -60,7 +60,7 @@ const router = express.Router();
             maxAge: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
             httpOnly: false,
             sameSite: 'Lax',
-            domain: 'localhost:3000',
+            domain: true,
           });
           res.status(200).json({ message: "User signed in" });
           console.log("User signed in");
